b8bfef0c93c9997e999f8892daaa650715579dde
SaurabhBorwankar <borwankar.saurabh@gmail.com>
2016-10-04 20:21:38 -0700

    Updated BigNum

--- BigNum.hs
+++ BigNum.hs
@@ -1,9 +1,7 @@
 {-
   Name: Saurabh Borwankar
-  Class: CS 252
-  Assigment: HW1
   Date: 02/15/2016
-  Description: This program performs arithmatic operations like addition,subtraction, multiplication and powerof on big numbers.  
+  Description: This program performs arithmatic operations like addition,subtraction, multiplication and powerof on big numbers.
 -}
 
 module BigNum (
@@ -28,12 +26,12 @@ bigAdd :: BigNum -> BigNum -> BigNum
 bigAdd x y = bigAdd' x y 0
 
 bigAdd' :: BigNum -> BigNum -> Block -> BigNum
-bigAdd' [] [] carry = if carry == 1 then [1] else []  
+bigAdd' [] [] carry = if carry == 1 then [1] else []
 bigAdd' (x:xs) ([]) 0 = [x] ++ bigAdd' xs [] 0
 bigAdd' (x:xs) ([]) 1 = if x+1 >999 then [(x+1) `mod` 1000 ] ++ bigAdd' xs [] 1 else [(x+1)] ++ bigAdd' xs [] 0
 bigAdd' ([]) (y:ys) 0 = [y] ++ bigAdd' [] ys 0
 bigAdd' ([]) (y:ys) 1 = if(y+1)>999 then [(y+1) `mod` 1000 ] ++ bigAdd' [] ys 1 else [(y+1)] ++ bigAdd' [] ys 0
-bigAdd' (x:[]) (y:[]) 0 = if x+y >999 then [ (x + y) `mod` 1000 ] ++ bigAdd' [] [] 1 else [ x+y ] 
+bigAdd' (x:[]) (y:[]) 0 = if x+y >999 then [ (x + y) `mod` 1000 ] ++ bigAdd' [] [] 1 else [ x+y ]
 bigAdd' (x:[]) (y:[]) 1 = if x+y > 999 then [(x + y + 1) `mod` 1000 ] ++ [1] else [ x + y + 1 ]
 bigAdd' (x:xs) (y:ys) z = if x + y > 999 then [(x + y + z) `mod` 1000 ] ++ (bigAdd' xs ys 1) else [ x + y + z ] ++ ( bigAdd' xs ys 0)
 
@@ -57,7 +55,7 @@ bigSubtract' ([x]) [] 1 = [x-1]
 bigSubtract' ([x]) [] 0 = [x]
 bigSubtract' ([x]) ([y]) 0 = if x-y < 0 then error "Negative numbers not supported " else [ x - y ]
 bigSubtract' ([x]) ([y]) 1 = if (x-y-1) < 0 then error "Negative numbers not supported" else [ (x - y - 1) ]
-bigSubtract' (x:xs) (y:ys) z = if x - y - z < 0 then [ (x - y - z + 1000) ] ++ (bigSubtract' xs ys 1) else [ x - y - z] ++ (bigSubtract' xs ys 0) 
+bigSubtract' (x:xs) (y:ys) z = if x - y - z < 0 then [ (x - y - z + 1000) ] ++ (bigSubtract' xs ys 1) else [ x - y - z] ++ (bigSubtract' xs ys 0)
 
 bigEq :: BigNum -> BigNum -> Bool
 bigEq [] [] = error "Empty List"
@@ -81,7 +79,7 @@ bigPowerOf :: BigNum -> BigNum -> BigNum
 bigPowerOf [] [] = error "Empty List"
 bigPowerOf x [0] = [1]
 bigPowerOf x [1] = x
-bigPowerOf x y = bigMultiply x (bigPowerOf x (bigDec y)) 
+bigPowerOf x y = bigMultiply x (bigPowerOf x (bigDec y))
 --bigPowerOf _ _ = error "Your code here"
 
 prettyPrint :: BigNum -> String
@@ -107,4 +105,3 @@ stringToBigNum' [] = []
 stringToBigNum' s | length s <= 3 = read (reverse s) : []
 stringToBigNum' (a:b:c:rest) = block : stringToBigNum' rest
   where block = read $ c:b:a:[]
-
